0c8a6866e80bdcd667b87d32077ad9d8192f0492
SilverWingedSeraph <silverwingedseraph@gmail.com>
2015-10-09 22:38:26 -0700

    Add rustlang support via pathogen.wq

--- init.sh
+++ init.sh
@@ -2,3 +2,5 @@
 
 # Install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+# Add Rustlang support for vim
+git clone --depth=1 https://github.com/rust-lang/rust.vim.git ~/.vim/bundle/rust.vim
