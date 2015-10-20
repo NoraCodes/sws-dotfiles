fd8ad2aa131d3d47476fabf5bb54e959b1bd3e57
SilverWingedSeraph <evessarudem@gmail.com>
2015-10-20 11:14:25 -0700

    More verbose init script.

--- init.sh
+++ init.sh
@@ -1,6 +1,17 @@
 #! /bin/bash
 
 # Install pathogen
+echo "Downloading and installing pathogen..."
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 # Add Rustlang support for vim
+echo "Adding Rustlang support."
 git clone --depth=1 https://github.com/rust-lang/rust.vim.git ~/.vim/bundle/rust.vim
+
+# Link .vimrc and .bashrc
+echo "Installing .vimrc"
+mv ~/.vimrc ~/.vimrc.bak
+cp ./.vimrc ~/.vimrc
+echo "Installing .bashrc"
+mv ~/.bashrc ~/.bashrc.bak
+cp ./.bashrc ~/.bashrc
