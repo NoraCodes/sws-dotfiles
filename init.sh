2df96e4f432eadd087743b3ae251e898ef4b8e4a
SilverWingedSeraph <lfstindall@gmail.com>
2015-11-28 09:58:24 -0800

    Remove broken python plugin.

--- init.sh
+++ init.sh
@@ -8,11 +8,6 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vi
 echo "Adding Rustlang support."
 git clone --depth=1 https://github.com/rust-lang/rust.vim.git ~/.vim/bundle/rust.vim
 
-# Add Python support for vim
-echo "Adding Python support."
-git clone https://github.com/rkulla/pydiction.git ~/.vim/bundle/pydiction.vim
-mv ~/.vim/bundle/pydiction.vim/after/ftplugin/ ~/.vim/
-
 # Install oh-my-zsh
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
