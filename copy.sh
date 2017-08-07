be46d61a4d1832417eaa39e0e4633f09b58a0ee0
SilverWingedSeraph <lfstindall@gmail.com>
2017-08-07 14:06:24 -0700

    Add terminalrc for XFCE

--- copy.sh
+++ copy.sh
@@ -1,21 +1,32 @@
 #! /bin/bash
 
 # Link .vimrc and .bashrc
+cdir=$(pwd)
+
 echo "Installing vimrc"
 mv ~/.vimrc ~/.vimrc.bak
-ln -sf ./vimrc ~/.vimrc
+ln -s $cidr/vimrc ~/.vimrc
+
 echo "Installing bashrc"
 mv ~/.bashrc ~/.bashrc.bak
-ln -sf ./bashrc ~/.bashrc
+ln -s $cidr/bashrc ~/.bashrc
+
 echo "Installing zshrc"
 mv ~/.zshrc ~/.zshrc.bak
-ln -sf ./zshrc ~/.zshrc
+ln -s $cdir/zshrc ~/.zshrc
+
 echo "Installing gitconfig"
 mv ~/.gitconfig ~/.gitconfig.bak
-ln -sf ./gitconfig ~/.gitconfig
+ln -s $cdir/gitconfig ~/.gitconfig
+
 echo "Installing tmux.conf"
 mv ~/.tmux.conf ~/.tmux.conf.bak
-ln -sf ./tmux.conf ~/.tmux.conf
+ln -s $cdir/tmux.conf ~/.tmux.conf
+
 echo "Installing gdbinit"
 mv ~/.gdbinit ~/.gdbinit.bak
-ln -sf ./gdbinit ~/.gdbinit
+ln -s $cdir/gdbinit ~/.gdbinit
+
+echo "Installing xfce4 terminalrc"
+mv ~/.config/xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc.bak
+ln -s $cdir/config/xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc
