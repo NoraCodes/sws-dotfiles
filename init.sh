ee9295c1488d889a652ba8fcec183f98d0fa7650
SilverWingedSeraph <lfstindall@gmail.com>
2017-09-16 11:39:21 -0500

    Remove redundant init steps

--- init.sh
+++ init.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Install pathogen
-echo "Downloading and installing pathogen..."
-mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-# Add Rustlang support for vim
-echo "Adding Rustlang support."
-git clone --depth=1 https://github.com/rust-lang/rust.vim.git ~/.vim/bundle/rust.vim
-
 # Install oh-my-zsh
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
